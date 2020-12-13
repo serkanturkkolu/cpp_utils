7e03b06913449bec8da114add00a6095bf91fa29
Serkan TURKKOLU <serkanturkkolu@gmail.com>
2020-12-13 18:20:45 +0300

    Update parallelAccumulate.hpp

--- parallelAccumulate.hpp
+++ parallelAccumulate.hpp
@@ -6,17 +6,17 @@
 
 
 template< class InputIt, class T, class BinaryOperation >
-T parallelAccumulate(InputIt&& first, InputIt&& last, T init, BinaryOperation&& biOp ){
+T parallelAccumulate(InputIt first, InputIt last, T init, BinaryOperation biOp ){
     int dist=std::distance(first,last);
     int coreCount = std::thread::hardware_concurrency();
     std::future<T> th[coreCount];
     for(int i=0;i<coreCount;++i){
         if(i!=coreCount-1){
-            th[i]=std::move(std::async(std::launch::async,[&first,&last,&biOp,coreCount,dist,i]{return std::accumulate(std::next(std::forward<InputIt>(first),(dist*i/coreCount)),std::next(std::forward<InputIt>(first),(dist*(i+1)/coreCount)),0,std::forward<BinaryOperation>(biOp));}));
+            th[i]=std::move(std::async(std::launch::async,[&first,&last,&biOp,coreCount,dist,i]{return std::accumulate(std::next(first,(dist*i/coreCount)),std::next(first,(dist*(i+1)/coreCount)),0,biOp);}));
             
         }else
         {
-            th[i]=std::move(std::async(std::launch::async,[&first,&last,&biOp,coreCount,dist,i]{return std::accumulate(std::next(std::forward<InputIt>(first),(dist*i/coreCount)),std::forward<InputIt>(last),0,std::forward<BinaryOperation>(biOp));}));
+            th[i]=std::move(std::async(std::launch::async,[&first,&last,&biOp,coreCount,dist,i]{return std::accumulate(std::next(first,(dist*i/coreCount)),last,0,biOp);}));
             
         }
         
@@ -28,7 +28,7 @@ T parallelAccumulate(InputIt&& first, InputIt&& last, T init, BinaryOperation&& 
 }
 
 template< class InputIt, class T, class BinaryOperation >
-T pureParallelAccumulate(InputIt&& first, InputIt&& last, T init, BinaryOperation&& biOp ){ //It doesn't use std::accumulate STL function. But low performance.
+T pureParallelAccumulate(InputIt first, InputIt last, T init, BinaryOperation biOp ){ //It doesn't use std::accumulate STL function. But low performance.
     auto local_accumulate = [](InputIt first,InputIt last,T init, BinaryOperation biOp){
         T res{};    
         for(auto iter=first;iter!=last;++iter){
@@ -42,11 +42,11 @@ T pureParallelAccumulate(InputIt&& first, InputIt&& last, T init, BinaryOperatio
     
     for(int i=0;i<coreCount;++i){
         if(i!=coreCount-1){
-            th[i]=std::move(std::async(std::launch::async,local_accumulate,std::next(std::forward<InputIt>(first),(dist*i/coreCount)),std::next(std::forward<InputIt>(first),(dist*(i+1)/coreCount)),0,std::forward<BinaryOperation>(biOp)));
+            th[i]=std::move(std::async(std::launch::async,local_accumulate,std::next(first,(dist*i/coreCount)),std::next(first,(dist*(i+1)/coreCount)),0,biOp));
             
         }else
         {
-            th[i]=std::move(std::async(std::launch::async,local_accumulate,std::next(std::forward<InputIt>(first),(dist*i/coreCount)),std::forward<InputIt>(last),0,std::forward<BinaryOperation>(biOp)));
+            th[i]=std::move(std::async(std::launch::async,local_accumulate,std::next(first,(dist*i/coreCount)),last,0,biOp));
             
         }
         
